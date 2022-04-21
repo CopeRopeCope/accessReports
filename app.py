dfd51e9ae5181f35e1249db072ee410feea98b0a
Petar <petarnrt4811@gmail.com>
2022-04-21 19:01:42 +0200

    flask app and modify backand

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+import pandas as pd
+import notInFacility
+
+app=Flask(__name__)
+
+@app.route('/', methods=['GET','POST'])
+
+
+def home():
+    return render_template("index.html")
+
+
+@app.route("/data", methods=['GET', 'POST'])
+def data():
+    if request.method == 'POST':
+        file = request.form['upload-file']
+        #data = pd.read_excel(file)
+        data = notInFacility.calculate(file)
+        return render_template('data.html', data=data)
+        
+
+
+if __name__ == "__main__":
+    app.run()
--- notInFacility.py
+++ notInFacility.py
@@ -8,37 +8,46 @@ def getDate(x):
 
 
 
-
-print ('**********************************************')
-a = pd.read_excel (r'Peca_swipe.xls')
-df = a.sort_values(by=['DateTime'])
-#print (df[df["DateTime"] == "2022-03-25 19:17:46 Friday"])
-for i,r in df.iterrows():
-    if (r['Addr'] in  "Prod. Exit-Exit") or (r['Addr'] in  "Main Entry-Exit"):
-        dateTimeExit = r['DateTime']
-        doorExit = r['Addr']
-        dayExit = dateTimeExit.split(" ")[-1]
-        dateExit = getDate(dateTimeExit)
-        
-        for x,p in df.iterrows():
-        #if (len(df.index)-1) > int(i):
-            # doorIn = df.loc[[int(i)+1],'Addr'].values[0]
-            doorIn = p['Addr']
-            if "-In" in doorIn:
-                out = False
-                #dateTimeIn = df.loc[[int(i)+1],['DateTime']].values[0]
-                dateTimeIn = p['DateTime']
-                dayIn = dateTimeIn[0].split(" ")[-1]
-                dateIn = getDate(dateTimeIn)
-                if dateExit < dateIn:
-                    print (dateExit,doorExit )
-                    print (dateIn, doorIn)
-                    out = dateIn - dateExit
-                    print ( out)
-                    print ("\n")
-            if out :
-                break            
-
+def calculate (file):
+    print ('**********************************************')
+    a = pd.read_excel (r'Peca_swipe.xls')
+    df = a.sort_values(by=['DateTime'])
+    inout =[]
+    #print (df[df["DateTime"] == "2022-03-25 19:17:46 Friday"])
+    for i,r in df.iterrows():
+        if (r['Addr'] in  "Prod. Exit-Exit") or (r['Addr'] in  "Main Entry-Exit"):
+            dateTimeExit = r['DateTime']
+            doorExit = r['Addr']
+            dayExit = dateTimeExit.split(" ")[-1]
+            dateExit = getDate(dateTimeExit)
+            
+            for x,p in df.iterrows():
+            #if (len(df.index)-1) > int(i):
+                # doorIn = df.loc[[int(i)+1],'Addr'].values[0]
+                doorIn = p['Addr']
+                if "-In" in doorIn:
+                    out = False
+                    #dateTimeIn = df.loc[[int(i)+1],['DateTime']].values[0]
+                    dateTimeIn = p['DateTime']
+                    dayIn = dateTimeIn[0].split(" ")[-1]
+                    dateIn = getDate(dateTimeIn)
+                    if dateExit < dateIn:
+                        out = dateIn - dateExit
+                        inout.append (i)
+                        inout.append (dateExit)
+                        inout.append (doorExit)
+                        inout.append (dateIn)
+                        inout.append (out)
+                        #print (dateExit,doorExit )
+                        #print (dateIn, doorIn)
+                        
+                        #print ( out)
+                        #print ("\n")
+                if out :
+                    break            
+    print ('---------------------------------------------------')
+    print (inout)
+    return inout
             
         #break
 #print (len(df.index))
